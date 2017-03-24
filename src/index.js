7a1369f70e582242f41bac1a31f5154e1fe3c4d1
dhmoon91 <dhmoon91@gmail.com>
2017-03-24 08:18:06 -0400

    fixed index.js to route to /

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import {Route, Router, IndexRoute, browserHistory} from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,7 +11,14 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Provider store={createStoreWithMiddleware(reducers)}>
+  <Router history={browserHistory}>
+
+
+    <Route path="/" component = {App} />
+
+
+</Router>
   </Provider>
-  , document.querySelector('.container'));
+  , document.querySelector('.container')
+);
